026f9f36d3dbb2295b7c4ff9e46f43c2fe3ab1be
wyd <wyudi@qq.com>
2020-08-09 00:12:13 +0800

    second commit： 1/jdk8->jdk11 2/jdk8兼容性未测试

---
+++ src/main/java/io/lazyii/captcha/CircleTextCaptchaService.java
@@ -0,0 +1,9 @@
+package io.lazyii.captcha;
+
+/**
+ * @author wyd
+ * @time 2020/8/8 10:27 下午
+ * @description
+ */
+public class CircleTextCaptchaService implements CaptchaService{
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author wyd
+ * @time 2020/8/8 10:13 下午
+ * @description
+ */
+module human.captcha {
+    requires java.desktop;
+
+    exports io.lazyii.captcha;
+}
